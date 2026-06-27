c723e0d954b14be06deec50edc720f50fa5c5e30
peter_lpt <peter_lpt@163.com>
2026-06-27 21:26:35 +0800

    chore: add dev/build launch scripts

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+set RUSTUP_HOME=D:\environment\rust\.rustup
+set CARGO_HOME=D:\environment\rust\.cargo
+set PATH=%CARGO_HOME%\bin;%PATH%
+pnpm tauri build
---
+++ build.ps1
@@ -0,0 +1,4 @@
+$env:RUSTUP_HOME = "D:\environment\rust\.rustup"
+$env:CARGO_HOME = "D:\environment\rust\.cargo"
+$env:PATH = "$env:CARGO_HOME\bin;$env:PATH"
+pnpm tauri build
